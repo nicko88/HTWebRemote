89411d6b141511e4580377fd664aa854b04dea30
Nicholas Oldroyd <sirmasterboy@gmail.com>
2021-03-03 20:11:21 -0600

    Prevent crash in Wemo control when a request fails

--- HTPCRemote/Devices/Controllers/WemoPlugControl.cs
+++ HTPCRemote/Devices/Controllers/WemoPlugControl.cs
@@ -20,47 +20,47 @@ namespace HTPCRemote.Devices.Controllers
 
 		private static void SendRequest(string action)
 		{
-			string TARGETSTATUS = "0";
-			if (action == "on")
-			{
-				TARGETSTATUS = "1";
-			}
-
-			HttpWebRequest req = WebRequest.Create($"http://{_IP}:{49153}/upnp/control/basicevent1") as HttpWebRequest;
-			string reqContent = @"<?xml version=""1.0"" encoding=""utf-8""?>";
-			reqContent += @"<s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"" s:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/"">";
-			reqContent += "<s:Body>";
-
-
-			if (action != "toggle")
-			{
-				reqContent += @"<u:SetBinaryState xmlns:u=""urn:Belkin:service:basicevent:1"">";
-				reqContent += $"<BinaryState>{TARGETSTATUS}</BinaryState>";
-				reqContent += "</u:SetBinaryState>";
-				req.Headers.Add(@"SOAPACTION:""urn:Belkin:service:basicevent:1#SetBinaryState""");
-			}
-			else
-			{
-				reqContent += @"<u:GetBinaryState xmlns:u=""urn:Belkin:service:basicevent:1"">";
-				reqContent += "<BinaryState>1</BinaryState>";
-				reqContent += "</u:GetBinaryState>";
-				req.Headers.Add(@"SOAPACTION:""urn:Belkin:service:basicevent:1#GetBinaryState""");
-			}
-
-			reqContent += "</s:Body>";
-			reqContent += "</s:Envelope>";
-			UTF8Encoding encoding = new UTF8Encoding();
-
-			req.ContentType = @"text/xml; charset=""utf-8""";
-			req.Method = "POST";
-
-			using (Stream requestStream = req.GetRequestStream())
-			{
-				requestStream.Write(encoding.GetBytes(reqContent), 0, encoding.GetByteCount(reqContent));
-			}
-
 			try
 			{
+				string TARGETSTATUS = "0";
+				if (action == "on")
+				{
+					TARGETSTATUS = "1";
+				}
+
+				HttpWebRequest req = WebRequest.Create($"http://{_IP}:{49153}/upnp/control/basicevent1") as HttpWebRequest;
+				string reqContent = @"<?xml version=""1.0"" encoding=""utf-8""?>";
+				reqContent += @"<s:Envelope xmlns:s=""http://schemas.xmlsoap.org/soap/envelope/"" s:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/"">";
+				reqContent += "<s:Body>";
+
+
+				if (action != "toggle")
+				{
+					reqContent += @"<u:SetBinaryState xmlns:u=""urn:Belkin:service:basicevent:1"">";
+					reqContent += $"<BinaryState>{TARGETSTATUS}</BinaryState>";
+					reqContent += "</u:SetBinaryState>";
+					req.Headers.Add(@"SOAPACTION:""urn:Belkin:service:basicevent:1#SetBinaryState""");
+				}
+				else
+				{
+					reqContent += @"<u:GetBinaryState xmlns:u=""urn:Belkin:service:basicevent:1"">";
+					reqContent += "<BinaryState>1</BinaryState>";
+					reqContent += "</u:GetBinaryState>";
+					req.Headers.Add(@"SOAPACTION:""urn:Belkin:service:basicevent:1#GetBinaryState""");
+				}
+
+				reqContent += "</s:Body>";
+				reqContent += "</s:Envelope>";
+				UTF8Encoding encoding = new UTF8Encoding();
+
+				req.ContentType = @"text/xml; charset=""utf-8""";
+				req.Method = "POST";
+
+				using (Stream requestStream = req.GetRequestStream())
+				{
+					requestStream.Write(encoding.GetBytes(reqContent), 0, encoding.GetByteCount(reqContent));
+				}
+			
 				HttpWebResponse response = req.GetResponse() as HttpWebResponse;
 				using (Stream rspStm = response.GetResponseStream())
 				{
